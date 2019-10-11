f9290abdd3b21df18646d9c9f4c3720d40b65234
wilson <whuanca06@gmail.com>
2019-10-11 14:15:02 -0400

    adding alerts

--- componentes/lib/src/pages/alert_page.dart
+++ componentes/lib/src/pages/alert_page.dart
@@ -9,6 +9,48 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('alert'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('mostar alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape:  StadiumBorder(),
+          onPressed: ()=>_MostarAlerta(context),
+        ),
+
+      ),
+    );
+  }
+
+  void _MostarAlerta(BuildContext context){
+    
+    showDialog(
+      
+    context: context,
+    barrierDismissible: true,
+    builder: (context){
+      return AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+        title: Text('Titulo'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text('este es el contenido de  la caja de la alerta'),
+            FlutterLogo( size: 100.0,),
+          ],
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Cancelar'),
+            onPressed: ()=> Navigator.of(context).pop(),
+          ),
+          FlatButton(
+            child: Text('OK'),
+            onPressed: (){ Navigator.of(context).pop();},
+          ),
+        ],
+      );
+    }
     );
   }
 }
