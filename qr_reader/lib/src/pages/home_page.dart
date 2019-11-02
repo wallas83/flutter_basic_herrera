0e30e7a57eb7925cd28f096f30171ac0784efa27
wilson <whuanca06@gmail.com>
2019-11-02 00:42:42 -0400

    adding streamBuilder

--- qr_reader/lib/src/pages/home_page.dart
+++ qr_reader/lib/src/pages/home_page.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:qr_reader/src/model/scan_model.dart';
 import 'package:qr_reader/src/pages/direcciones_page.dart';
 import 'package:qr_reader/src/pages/mapas_page.dart';
+import 'package:qr_reader/src/pages/scans_bloc.dart';
 import 'package:qrcode_reader/qrcode_reader.dart';
 
 class HomePage extends StatefulWidget {
-
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
-  int currentIndex =  0;
+  final scansBloc = new ScansBloc();
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,67 +21,69 @@ class _HomePageState extends State<HomePage> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.delete_forever),
-            onPressed: (){},
+            onPressed: scansBloc.borrarScanTODOS,
           )
         ],
       ),
-        body: _callPage(currentIndex),
-         
-          bottomNavigationBar: _crearBotomNavigationbar(),
-          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-          floatingActionButton: _crearFloatingButtom(),
-      );
+      body: _callPage(currentIndex),
+      bottomNavigationBar: _crearBotomNavigationbar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: _crearFloatingButtom(),
+    );
   }
 
   Widget _crearBotomNavigationbar() {
     return BottomNavigationBar(
       currentIndex: currentIndex,
-      onTap: (index){ setState(() {
-        currentIndex = index;
-      });},
-      items: [BottomNavigationBarItem(
-        icon: Icon(Icons.map),
-        title: Text('mapas')
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.brightness_5),
-        title: Text('Direcciones')
-      )],
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.map), title: Text('mapas')),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.brightness_5), title: Text('Direcciones'))
+      ],
     );
   }
 
- Widget  _callPage(int paginaActual) {
-  switch (paginaActual) {
-    case 0: return MapasPage();
-    case 1: return DireccionPage();
+  Widget _callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionPage();
 
-    default:
-    return MapasPage();
+      default:
+        return MapasPage();
+    }
   }
- }
 
   Widget _crearFloatingButtom() {
     return FloatingActionButton(
-          child: Icon(Icons.filter_center_focus),
-          onPressed: _scanQR ,
-          backgroundColor: Theme.of(context).primaryColor,
+      child: Icon(Icons.filter_center_focus),
+      onPressed: _scanQR,
+      backgroundColor: Theme.of(context).primaryColor,
     );
   }
 
-   _scanQR() async {
-     //https://www.google.com/
-     //geo:-16.502047, -68.1313,500
-      String futureString  = 'https://www.google.com/';
+  _scanQR() async {
+    //https://www.google.com/
+    //geo:-16.502047, -68.1313,500
+    String futureString = 'https://www.google.com/';
 
     //  try {
-    //     futureString = await new QRCodeReader().scan();  
+    //     futureString = await new QRCodeReader().scan();
     //  } catch (e) {
     //       futureString = e.toString();
-    //  }    
+    //  }
     // print(' futureString $futureString');
 
-    // if(futureString != null) {
-    //   print("tenemos informacion");
-    // }
+    if (futureString != null) {
+      final scan = ScanModel(valor: futureString);
+        scansBloc.agregarScan(scan);
+       }
+    
   }
 }
---
+++ qr_reader/lib/src/pages/scans_bloc.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:qr_reader/src/model/scan_model.dart';
+import 'package:qr_reader/src/provider/db_provider.dart';
+
+class ScansBloc {
+
+  static final ScansBloc _singleton =  new ScansBloc._internal();
+
+  factory ScansBloc() {
+    return _singleton;
+  }
+
+  ScansBloc._internal(){
+      //obtener los scans de la base de datos 
+      obtenerScans();
+  }
+
+  final _scansController = StreamController<List<ScanModel>>.broadcast();
+ 
+  Stream<List<ScanModel>> get scansStream => _scansController.stream;
+
+  void dispose() { 
+      _scansController?.close();   
+    }
+
+
+    obtenerScans() async{
+      //se adiciona los datos al sink
+      _scansController.sink.add(await DBProvider.db.getTodosScans());
+    }
+
+    agregarScan(ScanModel scan)async {
+      await DBProvider.db.nuevoScan(scan);
+      obtenerScans();
+    }
+    borrarScan(int id) async{
+     await  DBProvider.db.deleteScan(id);
+     obtenerScans();
+    }
+
+    borrarScanTODOS () async{
+          await DBProvider.db.deleteAll();
+          obtenerScans();
+    }
+}
+
