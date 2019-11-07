2dc19e5595a516755a8220f4a0221a47a5ecad86
wilson <whuanca06@gmail.com>
2019-11-07 14:01:17 -0400

    adding login

--- formulario_block/lib/src/pages/login_page.dart
+++ formulario_block/lib/src/pages/login_page.dart
@@ -1,13 +1,145 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+      body: 
+      SingleChildScrollView(
+              child: Stack(
+          children: <Widget>[
+            _crearFondo(context),_loginForm(context)],
+        ),
       ),
+    );
+  }
+
+  Widget _crearFondo(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final fondoMorado = Container(
+      height: size.height * 0.4,
+      width: double.infinity,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: <Color>[
+        Color.fromRGBO(63, 63, 156, 1.0),
+        Color.fromRGBO(90, 70, 178, 1.0)
+      ])),
+    );
+    final circulo = Container(
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100.0),
+          color: Color.fromRGBO(255, 255, 255, 0.09)),
+    );
+    return Stack(
+      children: <Widget>[
+        fondoMorado,
+        Positioned(top: 90.0, left: 30.0,width:20.0,height: 20.0, child: circulo),
+        Positioned(top: -40.0, left: -30.0, child: circulo),
+        Positioned(top: 230.0, left: 50.0,width:30.0,height: 30.0, child: circulo),
+        Positioned(top: 80.0, right: 50.0,width: 50.0,height: 50.0, child: circulo),
+        Positioned(top: 180.0, right:-50.0, child: circulo),
+        Container(
+          padding: EdgeInsets.only(top: 80.0),
+          child: Column(
+            children: <Widget>[
+              Icon(Icons.person_pin_circle, color: Colors.white, size: 100.0,),
+              SizedBox(height: 10.0, width: double.infinity,),
+              Text('Wilson Huanca', style: TextStyle(color: Colors.white, fontSize: 25.0),)
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+   Widget _loginForm(BuildContext context) {
+     final size = MediaQuery.of(context).size;
+      return SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SafeArea(
+              child: Container(
+                height: 180.0,
+              ),
+            ),
+            Container(
+              width: size.width * 0.85,
+              margin: EdgeInsets.symmetric(vertical: 30.0),
+              padding: EdgeInsets.symmetric(vertical: 50.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5.0),
+                boxShadow: <BoxShadow> [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 3.0,
+                    offset: Offset(0.0, 5.0),
+                    spreadRadius: 3.0
+                  )
+                ]
+              ),
+              child: Column(
+                children: <Widget>[
+                  Text('Ingreso', style: TextStyle(fontSize: 20.0),),
+                  SizedBox(height: 60.0,),
+                  _crearEmail(),
+                   SizedBox(height: 30.0,),
+                  _crearpasword(),
+                   SizedBox(height: 30.0,),
+                   _crearBoton()
+                ],
+              ),
+            ),
+            Text('¿Olvidó su Contraseña?'),
+            SizedBox(height: 100.0,)
+          ],
+        ),
+        
+      );
+  }
+
+  Widget _crearEmail() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(Icons.alternate_email, color: Colors.deepPurple,),
+          hintText: 'ejemplo@correo.com',
+          labelText: 'Correo electronico'
+        ),
+      ),
+    );
+  }
+
+  Widget _crearpasword() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+      obscureText: true,
+        decoration: InputDecoration(
+          icon: Icon(Icons.lock_outline, color: Colors.deepPurple,),
+          labelText: 'Contraseña'
+        ),
+      ),
+    );
+  }
+
+   Widget _crearBoton() {
+    return RaisedButton(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 80.0,vertical: 30.0),
+        child: Text('Ingresar'),
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0)
+      ),
+      elevation: 0.0,
+      color: Colors.deepPurple,
+      textColor: Colors.white,
+      onPressed: (){},
     );
   }
 }
